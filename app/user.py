b3d69505b75c5aec0fddb10b3994b8f81c08cdc3
Widoyo <widoyo@gmail.com>
2025-06-12 15:06:55 +0700

    Tweak halaman User

--- app/user.py
+++ app/user.py
@@ -1,15 +1,17 @@
-from flask import Blueprint, render_template
-
+from flask import Blueprint, render_template, url_for, redirect
+from flask_login import login_required, current_user
 from app.models import User
 
 bp = Blueprint('user', __name__, url_prefix='/user')
 
 
 @bp.route('/')
+@login_required
 def index():
-    users = User.select()
+    if current_user.perusahaan:
+        return redirect(url_for('homepage'))
+    users = User.select().order_by(User.username.asc())
     ctx = {
         'users': users
     }
-    
     return render_template('user/index.html', ctx=ctx)
